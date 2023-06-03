ab7fe16b61ff127b411b25d043b6734ed316cb99
akmalirf <akmalirfansyah31@gmail.com>
2023-06-04 04:20:12 +0700

    move js

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function getRandomPokemon() {
+  const rand = Math.floor(Math.random() * 100) + 1;
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${rand}`;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const pokemonName = data.name;
+      const pokemonImages = data.sprites;
+      const pokemonAbilities = data.abilities;
+      const pokemonHeight = data.height;
+      const pokemonWeight = data.weight;
+
+      document.getElementById('pokemonName').innerHTML = `
+              <h1>${pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h1>
+            `;
+      document.getElementById('carouselInner').innerHTML = `
+              <div class="carousel-item active">
+                <img src="${pokemonImages.front_default}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${pokemonImages.back_default}" class="d-block w-100" alt="...">
+              </div>
+            `;
+      document.getElementById('abilitiesBody').innerHTML = '';
+      pokemonAbilities.forEach(ability => {
+        const listItem = document.createElement('li');
+        listItem.innerText = ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1);
+        document.getElementById('abilitiesBody').appendChild(listItem);
+      });
+      document.getElementById('physicalAttributesBody').innerHTML = `
+              <li>Height: ${pokemonHeight}</li>
+              <li>Weight: ${pokemonWeight}</li>
+            `;
+      document.getElementById('apiLink').href = apiUrl;
+    });
+}
+
+document.getElementById('gachaButton').addEventListener('click', getRandomPokemon);
